514343b44a06256e3bdb00935f2875207ab060ef
Guillermo <45288150+SokerOne@users.noreply.github.com>
2021-11-29 21:30:50 +0100

    [UPDATE] C++ P13

---
+++ Programacion_C++/GuillermoSuarezCalleja/GuillermoSuarezCalleja/Image.h
@@ -0,0 +1,45 @@
+#pragma once
+
+struct SImage
+{
+	int m_iResolutionX, m_iResolutionY;
+	int m_iColorDepth;
+	const char* bType;
+	int iAlpha;
+
+	SImage() 
+	{
+		m_iResolutionX = 1080;
+		m_iResolutionY = 720;
+		m_iColorDepth = 255;
+		bType = "image";
+		iAlpha = 255;
+	}
+	~SImage() { printf("Objeto de la clase SImage borrado."); }
+	int DrawRGBImage(unsigned char* pBuffer);
+};
+
+struct SJpg : public SImage
+{
+	SImage* m_oImage;
+	SJpg()
+	{
+		m_oImage = nullptr;
+		bType = "jpg";
+	}
+	~SJpg() { printf("Objeto de la clase JPG borrado."); }
+	int mDrawJPGImage(unsigned char* pBuffer);
+};
+
+struct SPng : public SImage
+{
+	SImage* m_oImage;
+	SPng()
+	{
+		m_oImage = nullptr;
+		bType = "png";
+	}
+	~SJpg() { printf("Objeto de la clase PNG borrado."); }
+	int mDrawPNGImage(unsigned char* pBuffer);
+	void mDeleteAlphaChannel(SImage**, int);
+};
---
+++ Programacion_C++/GuillermoSuarezCalleja/GuillermoSuarezCalleja/Practica13.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include"Image.h"
+
+
+int main()
+{
+	SImage* tabla[4];
+
+	tabla[0] = new SJpg();
+	tabla[1] = new SPng();
+	tabla[2] = new SPng();
+	tabla[3] = new SJpg();
+
+	int size = sizeof(tabla) / 4;
+
+	SPng* image = nullptr;
+
+	image->mDeleteAlphaChannel(tabla, size);
+
+	delete[] tabla;
+	tabla = nullptr;
+
+	return 0;
+}
