8a343a750fe01c935f517ee4fe5faeacfdb4d813
Guillermo <45288150+SokerOne@users.noreply.github.com>
2021-12-02 20:58:03 +0100

    [UPDATE] P2- Arquitectura
    Acabada

--- Estructuras/Practica2/1_Skeleton/swalib-master/common/logic.cpp
+++ Estructuras/Practica2/1_Skeleton/swalib-master/common/logic.cpp
@@ -1,17 +1,20 @@
 #include "gameManager.h"
-
+#include<iostream>
 extern tBalls balls;
 
-void gameManager::mLogic(float fixed)
+void gameManager::mLogic(float fixedVel)
 {
 	// Run balls
-	for (unsigned int i = 0; i < NUM_BALLS; i++) {
+	for (unsigned int i = 0; i < NUM_BALLS; i++) 
+	{
 		// New Pos.
-		vec2 newpos = balls[i].pos + balls[i].vel * fixed;
+		// multiplico por fixedVel para cambiar la velocidad de las bolas y *20 para que vayan mas rapido 
+		vec2 newpos = balls[i].pos + balls[i].vel * (fixedVel * 20); 
 
 		// Collision detection.
 		bool collision = false;
 		int colliding_ball = -1;
+
 		for (unsigned int j = 0; j < NUM_BALLS; j++) {
 			if (i != j) {
 				float limit2 = (balls[i].radius + balls[j].radius) * (balls[i].radius + balls[j].radius);
@@ -40,7 +43,4 @@ void gameManager::mLogic(float fixed)
 			balls[i].vel.y *= -1.0;
 		}
 	}
-
-	SYS_Pump();
-	// SYS_Sleep(17);	// To force 60 fps - Practica 2.0
 }
