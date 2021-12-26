142bb4e2d864c298329818ff5006412eb590fae9
Guillermo <45288150+SokerOne@users.noreply.github.com>
2021-12-26 16:09:11 +0100

    [asd]

--- Programacion-2D/Practica2/src/main.cpp
+++ Programacion-2D/Practica2/src/main.cpp
@@ -1,4 +1,5 @@
 #define LITE_GFX_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
 
 #include <litegfx.h>
 #include <glfw3.h>
@@ -6,6 +7,10 @@
 #include "vec2.h"
 #include <string>
 #include "stb_image.h"
+
+
+
+ltex_t* loadTexture(const char* _filename);
 
 int main() {
 	double xpos, ypos;
@@ -20,32 +25,43 @@ int main() {
 	vec2 cursorPosition;
 	vec2 center((float)(width / 2.f), (float)(height / 2.f));
 
+	ltex_t* wall = loadTexture("C:/Users/Soker/Documents/GitHub/Practicas_MASTER/Programacion-2D/Practica2/data/wall.jpg");
+	ltex_t* fire = loadTexture("data/fire.png");
+	ltex_t* grille = loadTexture("grille.png");
+	ltex_t* light = loadTexture("light.png");
+
 	GLFWwindow* window;
 
 	glfwInit();
-	
-
-	glfwGetTime();
-
 	if (!glfwInit())
 	{
 		printf("error init");
 	}
+	
 
 	window = glfwCreateWindow(width, height, "P1", NULL, NULL);
 	if (!window)
 	{
 		printf("error window");
 	}
+	glfwMakeContextCurrent(window);
+	lgfx_setup2d(width, height);
 
 	while (!glfwWindowShouldClose(window))
 	{
-		
+		glfwGetTime();
 		delta = glfwGetTime();
 
-		lgfx_setup2d(width, height);
-		lgfx_clearcolorbuffer(0.f, 0.f, 0.f);
-		
+		lgfx_clearcolorbuffer(1.f, 1.f, 1.f);
+
+		lgfx_setcolor(0.f, 0.f, 0.f, 1.f);
+		lgfx_drawrect(100, 100, 50, 50);
+		lgfx_setcolor(1.f, 1.f, 1.f, 1.f);
+
+
+		lgfx_setblend(BLEND_SOLID);
+		ltex_drawrotsized(wall, 0, 0, 0, 0, 0, width, height, 0, 0, width / wall->width, height / wall->height);
+
 
 		glfwGetCursorPos(window, &xposMouse, &yposMouse);
 		cursorPosition.x = xposMouse;
@@ -55,14 +71,12 @@ int main() {
 
 		textDist = center.distance(cursorPosition);
 
-		std::string title = "Distance: "
-			+ std::to_string(textDist)
-			/*+ ", Angle: "
-			+ std::to_string(textAngle)*/;
-
-		glfwSetWindowTitle(window, title.c_str());
-
-		glfwMakeContextCurrent(window);
+		// std::string title = "Distance: "
+		// 	+ std::to_string(textDist)
+		// 	/*+ ", Angle: "
+		// 	+ std::to_string(textAngle)*/;
+		// 
+		// glfwSetWindowTitle(window, title.c_str());
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -76,10 +90,16 @@ int main() {
 
   return 0;
 }
-ltex_t* loadTexture(const char*);
-ltex_t* loadTexture(const char* filename, int width, int height)
+
+ltex_t* loadTexture(const char* _filename)
 {
-	int channels = 0;
-	float* image = stbi_loadf(filename, &width, &height, &channels, STBI_rgb_alpha);
-	return image;
+	int* x = new int;
+	int* y = new int;
+
+	unsigned char* buffer = stbi_load(_filename, x, y, nullptr, 4);
+	ltex_t* tex = ltex_alloc(*x, *y, 1);
+	ltex_setpixels(tex, buffer);
+	stbi_image_free(buffer);
+
+	return tex;
 }
